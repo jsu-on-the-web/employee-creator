0f349a5fa83b6f903607930f16ee32a9c0ed8d5e
Janet Suryadi <janet@suryadi.id.au>
2023-10-11 12:28:12 +1100

    Adding Spring Boot template

---
+++ backend/employeecreator/src/test/java/com/jsu/employeecreator/EmployeecreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsu.employeecreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeecreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
