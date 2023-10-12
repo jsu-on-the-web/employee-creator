22e30696881ec1cea8dc7c01226fc1ff26a3db05
Janet Suryadi <janet@suryadi.id.au>
2023-10-12 15:59:59 +1100

    Scaffolding of SpringBoot tests

---
+++ backend/employeecreator/src/test/java/com/jsu/employeecreator/EmployeeControllerTest.java
@@ -0,0 +1,9 @@
+package com.jsu.employeecreator;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeControllerTest {
+    
+}
