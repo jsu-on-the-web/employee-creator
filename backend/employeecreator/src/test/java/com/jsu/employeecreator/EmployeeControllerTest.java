8d3ec69ccfc46bbd3c065178ed1fe2282b69dfa3
Janet Suryadi <janet@suryadi.id.au>
2023-10-14 16:38:34 +1100

    Adding some boilerplate for Spring Tests

--- backend/employeecreator/src/test/java/com/jsu/employeecreator/EmployeeControllerTest.java
+++ backend/employeecreator/src/test/java/com/jsu/employeecreator/EmployeeControllerTest.java
@@ -1,9 +1,28 @@
 package com.jsu.employeecreator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeControllerTest {
-    
+    // Uncomment these once they're scaffolded
+    // @Mock
+    // private EmplyeeRepository repo;
+
+    // @Mock
+    // private ModelMapper modelMapper;
+
+    // @Mock
+    // private EmployeeService service;
+
+    // private EmployeeController controller;
+
+    @BeforeEach
+    void setUp() {
+        // Setup the controller itself
+    }
+
+
 }
--- backend/employeecreator/src/test/java/com/jsu/employeecreator/EmployeeServiceTest.java
+++ backend/employeecreator/src/test/java/com/jsu/employeecreator/EmployeeServiceTest.java
@@ -1,9 +1,23 @@
 package com.jsu.employeecreator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTest {
-    
+    // Uncomment these once they're scaffolded
+    // @Mock
+    // private EmplyeeRepository repo;
+
+    // @Mock
+    // private ModelMapper modelMapper;
+
+
+    // private EmployeeService service;
+
+    @BeforeEach
+    void setUp() {
+        // Setup the service itself
+    }
 }
