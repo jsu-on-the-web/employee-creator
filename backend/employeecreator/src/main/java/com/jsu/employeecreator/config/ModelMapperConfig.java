17581fab9134e4862db69642a347ce44a21a1ff8
Janet Suryadi <janet@suryadi.id.au>
2023-10-18 16:25:32 +1100

    Adding configs and dependencies

---
+++ backend/employeecreator/src/main/java/com/jsu/employeecreator/config/ModelMapperConfig.java
@@ -0,0 +1,28 @@
+package com.jsu.employeecreator.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ModelMapperConfig {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		
+		// mapper will take in DTO (Create or update)
+		// will turn DTO into Post so I can save it in db
+		// replace the need for using constructors
+		// or setters
+		
+		ModelMapper mapper = new ModelMapper();
+		// set up my options here
+		// trim strings
+		mapper.typeMap(String.class, String.class).setConverter(new StringTrimConverter());
+		mapper.getConfiguration().setSkipNullEnabled(true);
+		return mapper;
+		
+	}
+
+}
---
+++ backend/employeecreator/src/main/java/com/jsu/employeecreator/converters/StringTrimConverter.java
@@ -0,0 +1,23 @@
+package com.jsu.employeecreator.converters;
+import org.modelmapper.Converter;
+import org.modelmapper.spi.MappingContext;
+
+public class StringTrimConverter implements Converter<String, String>{
+
+	/**
+	 * Convert the given MappingContext to a String.
+	 *
+	 * @param  context  the MappingContext to be converted
+	 * @return          the converted String, or null if the source is null
+	 */
+	@Override
+	public String convert(MappingContext<String, String> context) {
+		
+		if(context.getSource() == null) {
+			return null;
+		}
+		
+		return context.getSource().trim();
+	}
+
+}
